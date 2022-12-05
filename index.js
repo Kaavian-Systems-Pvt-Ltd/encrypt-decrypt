bb8efbdc355b794b9c773229a12a244aaaa09e11
Lakshmi <thangalakshmi.r@kaaviansys.com>
2022-12-05 15:34:41 +0530

    class working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const rsa = require('node-rsa');
 require('dotenv').config();
 module.exports = class npmModule{
- keys(x){
+  static keys(x){
     const key= new rsa();
     key.importKey(x);
     return (key);
@@ -11,8 +11,8 @@ module.exports = class npmModule{
  * @param {string} str value to be encrypted
  * @returns encrypted value
  */
-    encrypt(str){
-    const y= keys(`${process.env.PUBLIC_KEY}`)
+   static encrypt(str){
+    const y= npmModule.keys(`${process.env.PUBLIC_KEY}`)
     const a = y.encrypt(str,'base64');
     return a;
 }
@@ -21,8 +21,8 @@ module.exports = class npmModule{
  * @param {string} str encrypted value 
  * @returns decrypted value
  */
-     decrypt(str){
-    const y= keys(`${process.env.PRIVATE_KEY}`)
+    static decrypt(str){
+    const y= npmModule.keys(`${process.env.PRIVATE_KEY}`)
     const b = y.decrypt(str,'utf8');
     return b;
 }
