a9960afab98196110d414a2d2638d5a64593f43d
Lakshmi <thangalakshmi.r@kaaviansys.com>
2022-12-05 11:57:24 +0530

    added files

---
+++ index.js
@@ -0,0 +1,32 @@
+const rsa = require('node-rsa');
+
+ function keys(x){
+    const key= new rsa();
+    key.importKey(x);
+    return (key);
+}
+/**
+ * 
+ * @param {key} k node rsa public key
+ * @param {string} str value to be encrypted
+ * @returns encrypted value  
+ */
+function encryption(k,str){
+    const y= keys(k)
+    const a = y.encrypt(str,'base64');
+    return a;
+}
+/**
+ * 
+ * @param {key} k node rsa private key  
+ * @param {string} str encrypted value
+ * @returns decrypted value
+ */
+function decryption(str){
+    const y= keys(k)
+    const b = y.decrypt(str,'utf8');
+    return b;
+}
+module.exports={
+    encryption,decryption
+}
