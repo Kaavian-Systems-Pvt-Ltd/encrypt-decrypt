e840621b230b25dfa1ac01d75d3cf80b64c51d06
Lakshmi <thangalakshmi.r@kaaviansys.com>
2022-12-06 16:49:58 +0530

    changed package name

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-const Rsa = require('node-rsa');
-require('dotenv').config();
+const Rsa = require ('node-rsa');
+require ('dotenv').config ();
 
 module.exports = class npmModule {
   static keys(x) {
-    try {
-      const key = new Rsa();
-      key.importKey(x);
+    try{
+      const key = new Rsa ();
+      key.importKey (x);
       return (key);
-    } catch (err) {
-      throw new Error('Key must be a RSA key pair (or) Provide necessary keys ', err);
+    }catch (err) {
+      throw new Error ('Key must be a RSA key pair (or) Provide necessary keys ' , err);
     }
   }
 
   /**
  *
  * @param {string} str value to be encrypted
- * @returns encrypted value
+ * @returns encrypted value base64 format
  */
   static encrypt(str) {
     try {
-      const y = npmModule.keys(`${process.env.PUBLIC_KEY}`);
-      const a = y.encrypt(str, 'base64');
-      return a;
-    } catch (err) {
-      throw new Error('Please Provide the value to be encrypted', err);
+      const keyobject = npmModule.keys (`${process.env.PUBLIC_KEY}`);
+      return keyobject.encrypt (str , 'base64');
+    }catch (err) {
+      throw new Error ('Please Provide the value to be encrypted' , err);
     }
   }
 
   /**
  *
- * @param {string} str encrypted value
- * @returns decrypted value
+ * @param {string} str encrypted value base64 format
+ * @returns decrypted value in utf8 format
  */
   static decrypt(str) {
     try {
-      const y = npmModule.keys(`${process.env.PRIVATE_KEY}`);
-      const b = y.decrypt(str, 'utf8');
-      return b;
-    } catch (err) {
-      throw new Error('Please Provide the value to be decrypted ', err);
+      const keyobject = npmModule.keys (`${process.env.PRIVATE_KEY}`);
+      return keyobject.decrypt (str , 'utf8');
+    }catch (err) {
+      throw new Error ('Please Provide the value to be decrypted ' , err);
     }
   }
 };
