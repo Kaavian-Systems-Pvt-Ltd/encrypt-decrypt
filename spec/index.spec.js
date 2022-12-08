acc2d730a41010077dcda155029a2e49efa7444d
Lakshmi <thangalakshmi.r@kaaviansys.com>
2022-12-08 12:57:27 +0530

    changed test case

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -4,7 +4,7 @@ require ('dotenv').config ();
 const npmModule = require ('../index');
 const sandbox = sinon.createSandbox ();
 const files = require ('../index');
-const object = new npmModule ();
+ new npmModule ();
 
 describe ('Key Function' , ()=> {
     afterEach ( ()=> {
@@ -12,42 +12,42 @@ describe ('Key Function' , ()=> {
     });
 
     it ('Error in keys' , ()=> { 
-        sandbox.stub (object , 'keys').throws (new Error ('failed'));
+        sandbox.stub (files , 'keys').throws (new Error ('failed'));
         expect (()=> keys ()).toThrow ();
     });
 
     it ('Convert string to rsa key object for encryption' , async ()=> { 
-        sandbox.stub (object , 'keys');
+        sandbox.stub (files , 'keys').returns ('qwerftghjsxdcfvbnxxcvb');
         const value = files.keys (`${process.env.PUBLIC_KEY}`);
-        expect (value).toBe (value);
+        expect (value).toBe ('qwerftghjsxdcfvbnxxcvb');
     });
     it ('Convert string to rsa key object for decryption' , async ()=> { 
-        sandbox.stub (object , 'keys');
-        const value = await files.keys (`${process.env.PRIVATE_KEY}`);
-        expect (value).toBe (value);
+        sandbox.stub (files , 'keys').returns ('fghujdvbbsdxfghjkxcv');
+        const value = files.keys (`${process.env.PRIVATE_KEY}`);
+        expect (value).toBe ('fghujdvbbsdxfghjkxcv');
     });
  
     describe ('Encryption' , ()=> {
         it ('Encrypted  Successfully' , async ()=> {
-            sandbox.stub (object , 'encrypt');
-            const value = await files.encrypt ('hello');
-            expect (value).toBe (value);
+            sandbox.stub (files , 'encrypt').returns ('sdxcfvghjertgyhujxcvb');
+            const value = files.encrypt ('hello');
+            expect (value).toBe ('sdxcfvghjertgyhujxcvb');
         });
         it ('Data to be encrypted is not given' , async ()=> {
-            sandbox.stub (object , 'encrypt').throws (new Error ('Failed to encrypt'));
-            // const value = await files.encrypt ();
+            sandbox.stub (files , 'encrypt').throws (new Error ('Failed to encrypt'));
             expect (()=> encrypt ()).toThrow ();
         });
 
     });
     describe ('Decryption' , ()=> {
         it ('Decrypted  Successfully' , async ()=> {
-            sandbox.stub (object , 'decrypt');
-            const value = await files.decrypt ('VtnGSzWvRjp2BNJONp71GYFkcW9inn1qCTRGj48O6lmiWinwssKEQ3EoBb8RmtlEMkQe3XqV2+FAS/G/3WZKm8yHguGgVmId1Yc7nRZCLWMmOUdkcgUatpjL4ApFWb93/Hy1VvgtnGxia5Mme34O0qVkleCYnTqZ4ak7IW03yP0=');
-            expect (value).toBe (value);
+            // sandbox.stub (object , 'decrypt');
+            sandbox.stub (files , 'decrypt' ).returns ('hello');
+            const value = files.decrypt ('Vsdfghjkwedrftgyhjsdcfvgbhstyu=');
+            expect (value).toEqual ('hello');
         });
         it ('Data to be decrypted is not given' , async ()=> {
-            sandbox.stub (object , 'decrypt').throws (new Error ('Failed to decrypt'));
+            sandbox.stub (files , 'decrypt').throws (new Error ('Failed to decrypt'));
             expect (()=> decrypt ()).toThrow ();
         });
 
